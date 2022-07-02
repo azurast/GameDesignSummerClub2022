a55d242e6db1a58e4e735876b712424128342d49
januarelsan <januarelsan@gmail.com>
2022-07-02 14:43:40 +0700

    Add Flap Features

--- Assets/Scripts/Starter/BirdMovement.cs
+++ Assets/Scripts/Starter/BirdMovement.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 // All we need to know about MonoBehaviour is that it is a C# class that is recognized by Unity
 public class BirdMovement : MonoBehaviour {
     /* STEP 1 : Define the properties the bird needs*/
+    public float flapPower = 1;
+    private Rigidbody2D rigidbody2D;
 
     // Start is called before the first frame update
     void Start() {
         /* STEP 2 : Setting the properties */
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,10 +19,14 @@ public class BirdMovement : MonoBehaviour {
         /* STEP 3 : Detecting user input
             Whenever the frame changes, we want the bird to 'fly up' whenever we click on them 
         */
+        if(Input.GetMouseButtonDown(0))
+            Flap();
     }
 
     // Flap is the function where you define the physics of the bird
     void Flap() {
         /* STEP 4 : Make the bird fly with some math & physics! */
+        Debug.Log("Flap");
+        rigidbody2D.velocity = Vector2.up * flapPower;
     }
 }
